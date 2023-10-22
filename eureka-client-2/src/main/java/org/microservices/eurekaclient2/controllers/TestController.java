d3ab3a66a46586ab80e38f57977a599d9b8b2cb5
Alex <sanyaantonyuk@gmail.com>
2023-10-22 20:13:52 +0200

    Added new module - Eureka Client 2

---
+++ eureka-client-2/src/main/java/org/microservices/eurekaclient2/controllers/TestController.java
@@ -0,0 +1,14 @@
+package org.microservices.eurekaclient2.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class TestController {
+    @GetMapping("/name")
+    public String test() {
+        return "Alex";
+    }
+}
